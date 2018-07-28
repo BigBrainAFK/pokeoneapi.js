2f71184561c07baf5238a4ec6dfa3f9febb35432
Unknown <tony.langhammer@yahoo.de>
2018-07-28 11:54:11 +0200

    Added raw requests

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ class Router {
 		});
 	}
 
+	get raw() {
+		const rawHandler = this.rawHandler;
+		return new Proxy({}, {
+			get: (obj, prop) => rawHandler(prop)
+		});
+	}
+
 	async handler(name) {
 		if (!validRequests.includes(name)) return new RangeError(`'${name}' is not a valid request!`);
 
@@ -27,6 +34,23 @@ class Router {
 
 		return resBody.success ? resBody.response.online : resBody.response;
 	}
+
+	async rawHandler(name) {
+		if (!validRequests.includes(name)) return new RangeError(`'${name}' is not a valid request!`);
+
+		const reqBody = {
+			request: name
+		};
+
+		const response = await fetch('http://api.poke.one/', {
+			method: 'POST',
+			body: JSON.stringify(reqBody),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.catch(console.error);
+
+		return response;
+	}
 }
 
-module.exports = new Router().api;
+module.exports = Router;
